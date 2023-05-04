34119d51ba7b76a280b9dfc975816cdc4aa3bc96
Mahima Pawar <77070844+047-Mahima@users.noreply.github.com>
2023-05-05 00:02:15 +0530

    Update 2215. Find the Difference of Two Arrays.cpp

--- Leetcode/May Challenge/2215. Find the Difference of Two Arrays.cpp
+++ Leetcode/May Challenge/2215. Find the Difference of Two Arrays.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
         
@@ -47,5 +47,35 @@ public:
         res.push_back(temp1);
 
         return res;
+    }
+};*/
+
+//Method 2 :
+class Solution {
+public:
+    vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
+        
+       vector<int> temp1, temp2;
+
+       // Constructing an unordered_set from a vector
+       unordered_set<int> uset1(nums1.begin(), nums1.end()), uset2(nums2.begin(), nums2.end());
+
+       for(auto it : uset2)
+       {
+           if(uset1.find(it) == uset1.end())
+           {
+               temp1.push_back(it);
+           }
+       }
+
+       for(auto it :uset1)
+       {
+            if(uset2.find(it) == uset2.end())
+           {
+               temp2.push_back(it);
+           }
+       }
+
+       return {temp2, temp1};
     }
 };
